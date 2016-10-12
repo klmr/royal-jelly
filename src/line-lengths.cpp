82c06bf40a4b9975ca3c41df778ec7d179767df6
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-10-12 11:05:13 +0000

    Add source code of line length counter

---
+++ src/line-lengths.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+
+    auto chr = char{};
+    auto line_length = int{};
+
+    while (std::cin.get(chr)) {
+        if (chr == '\n') {
+            std::cout << line_length << '\n';
+            line_length = 0;
+        } else {
+            ++line_length;
+        }
+    }
+
+    if (std::cin.bad()) {
+        std::cerr << "Error reading input.\n";
+        return EXIT_FAILURE;
+    }
+}
